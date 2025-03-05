8e38b72652edd4f050c6b221c511d27cc335ec44
BriceInCode <brice.nanyang1@gmail.com>
2025-03-05 16:00:13 +0100

    Template HTML pour l'envoie des message OTP par mail

---
+++ resources/views/emails/otp.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Votre code OTP</title>
+</head>
+<body>
+    <h1>Votre code OTP</h1>
+    <p>Bonjour,</p>
+    <p>Votre code OTP pour valider votre session est : <strong>{{ $otp }}</strong></p>
+    <p>Merci de l'utiliser pour finaliser votre demande.</p>
+    <p>Cordialement,</p>
+    <p>L'équipe de support</p>
+</body>
+</html>
